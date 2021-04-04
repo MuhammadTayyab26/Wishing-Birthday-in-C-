a092b994699cbd87e27a443157aa7748a4f94506
MuhammadTayyab26 <bcsm-f16-526@superior.edu.pk>
2021-04-04 12:58:10 +0500

    Wishing birthday in C++

---
+++ Wishing.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <windows.h>
+//Sleep used to pause program for the given time. that is declard in windows.h header file.
+
+using namespace std;
+int main(){
+	int i=5;
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\t\t\t Hi";
+	Sleep(350);
+	cout << " lets";
+	Sleep(500);
+	cout << " Count";
+	Sleep(650);
+	cout << " From";
+	Sleep(800); 
+	cout << " 5";
+	Sleep(950);
+	cout << " to";
+	Sleep(1100);
+	cout << " 1";
+	Sleep(1250);
+	system("cls");
+	
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t 5"; 
+	Sleep(1400);
+	system("cls");
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t 4"; 
+	Sleep(1550);
+	system("cls");
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t 3"; 
+	Sleep(1700);
+	system("cls");
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t 2"; 
+	Sleep(1850);
+	system("cls");
+	cout << "\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t 1"; 
+	Sleep(2000);
+	system("cls");
+	
+	
+	
+	while(i==5){
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\t\t\t   Happy Birthday To You ..!!";
+	system("Color 0E"); 
+	system("Color 1D"); 
+	system("Color 2C"); 
+	system("Color 3B"); 
+	system("Color 4A"); 
+	system("Color 59"); 
+	system("Color 68"); 
+	system("Color 71"); 
+	system("Color 86");
+	system("Color 95"); 
+	system("Color A4");  
+	system("Color B3"); 
+	system("Color C2"); 
+	system("Color D1"); 
+	system("Color E0"); 
+	system("cls");
+	
+	}
+
+return 0;
+}
